9b43ca2b2f1586241a8a48b8470273805124b822
M.K.E Firma <85478005+mkefirma@users.noreply.github.com>
2022-09-26 10:09:08 +0200

    master

---
+++ index.php
@@ -0,0 +1,11 @@
+<?php
+
+    require './Config.php';
+    require './inc/components/UserData.php';
+    require './inc/components/ControllerData.php';
+    require './inc/components/Head.php';
+
+?>
+    
+</body>
+</html>
